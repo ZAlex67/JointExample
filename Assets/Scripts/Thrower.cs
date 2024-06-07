a017561566ba1101de7699eddacb09c1292a1d2d
Aleksandr <zabrodinalex67@gmail.com>
2024-06-07 15:35:11 +0900

    Fix joint

--- Assets/Scripts/Thrower.cs
+++ Assets/Scripts/Thrower.cs
@@ -1,54 +1,36 @@
-using System.Collections;
 using UnityEngine;
 
 public class Thrower : MonoBehaviour
 {
-    [SerializeField] private Transform _point;
-    [SerializeField] private Transform _startPoint;
     [SerializeField] private Transform _spawnPoint;
-    [SerializeField] private float _speed;
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private Sphere _sphere;
 
-    private Coroutine _coroutine;
     private Sphere _sphereNew;
+    private Vector3 _startPosition;
+    private Quaternion _startRotation;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _sphereNew = Instantiate(_sphere, _spawnPoint);
         _sphereNew.gameObject.SetActive(false);
+        _startPosition = transform.position;
+        _startRotation = transform.rotation;
     }
 
     public void StartMove()
     {
-        if (_coroutine != null)
-        {
-            StopCoroutine(_coroutine);
-        }
-
         _sphereNew.gameObject.SetActive(true);
         _sphereNew.transform.position = _spawnPoint.position;
-
-        _coroutine = StartCoroutine(MoverCube());
+        _rigidbody.isKinematic = false;
     }
 
     public void Restart()
     {
         _sphereNew.gameObject.SetActive(false);
-        transform.position = _startPoint.position;
-        transform.rotation = _startPoint.rotation;
+        transform.position = _startPosition;
+        transform.rotation = _startRotation;
         _rigidbody.isKinematic = true;
-    }
-
-    private IEnumerator MoverCube()
-    {
-        while (transform.position != _point.position)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, _point.position, _speed * Time.deltaTime);
-            yield return null;
-        }
-
-        _rigidbody.isKinematic = false;
     }
 }
